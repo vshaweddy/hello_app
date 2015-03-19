c88c9a1be3b17190b34d68c5caed15738d11f06b
vshaweddy <vshaweddy@gmail.com>
2015-03-19 15:25:50 -0400

    adding goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-    render text: "hello, world!"
+    render text: "&iexcl; Holla, mundo!"
   end
+
+  def goodbye
+    render text: "Goodbye!"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'application#hello'
+  root 'application#goodbye'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
